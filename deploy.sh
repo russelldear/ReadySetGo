d71b8c9edac64bcf5988131889a50a1e90062542
russelldear <russell.dear@gmail.com>
2018-12-14 13:07:52 +1300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,8 @@ docker build -t readysetgo .
 docker tag readysetgo:latest 540629508292.dkr.ecr.us-east-1.amazonaws.com/readysetgo:latest
 docker push 540629508292.dkr.ecr.us-east-1.amazonaws.com/readysetgo:latest
 
+add-apt-repository ppa:eugenesan/ppa
+apt-get update
 apt-get install jq -y
 
 TASK=$(aws ecs describe-task-definition --task-definition readysetgo --output json | jq '.taskDefinition.containerDefinitions')
